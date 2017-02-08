2ce47ff4b407aa2b83e9e1abc5526794405cf56d
aarondlopez <aarondlopez@gmail.com>
2017-02-07 19:34:26 -0800

    bullets updated

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from pygame.sprite import Group
+
 from settings import Settings
 
 from ship import Ship
@@ -18,18 +20,14 @@ def run_game():
 
     # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
-
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screeen visable
-        pygame.display.flip()
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- bullet.py
+++ bullet.py
@@ -10,13 +10,24 @@ class Bullet(Sprite):
         self.screen = screen
 
         # Create a bullet rect at (0,0) and then set correct position.
-        self.ret = pygame.Rect(0,0, ai_settings.bullet_width,
+        self.rect = pygame.Rect(0,0, ai_settings.bullet_width,
             ai_settings.bullet_height)
         self.rect.centerx = ship.rect.centerx
         self.rect.top = ship.rect.top
 
         # Store the bullet's position as a decimale value.
-        self.y = float(self.rect.top.y)
+        self.y = float(self.rect.y)
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """Move the bullet up the screen."""
+        # Update the decimal position of the bullet
+        self.y -= self.speed_factor
+        # Update the rect position.
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
