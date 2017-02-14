efc4bb262d5adb1dfbefef2c1bf357a6fdc9b6af
aarondlopez <aarondlopez@gmail.com>
2017-02-14 14:00:27 -0800

    shooting aliens

--- settings.py
+++ settings.py
@@ -13,10 +13,10 @@ class Settings():
 
         # Bullet settings
         self.bullet_speed_factor = 1
-        self.bullet_width = 3
+        self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 1
+        self.bullets_allowed = 5
 
         # Alien settings
         self.alien_speed_factor = 1
