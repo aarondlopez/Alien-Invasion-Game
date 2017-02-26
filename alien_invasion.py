11e2ee2c70fd8ba24926d24b966d24755ca7dc32
aarondlopez <aarondlopez@gmail.com>
2017-02-26 09:52:39 -0800

    update button

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,8 @@ from pygame.sprite import Group
 
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 
 import game_functions as gf
